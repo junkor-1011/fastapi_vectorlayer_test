c4ad4f940673f41f663db2004654c129e516f113
junkor-1011 <tomoyukizxy@gmail.com>
2020-08-18 02:42:31 +0900

    WIP: GeoJSON-VectorTileLayer

--- app/app/routers/api_gpd.py
+++ app/app/routers/api_gpd.py
@@ -5,9 +5,14 @@ test api using GeoPandas(NOT using DB)
 import json
 import math
 # import pathlib
+from typing import (
+    Optional,
+    List,
+)
 
 from fastapi import (
     APIRouter,
+    HTTPException,
 )
 import geopandas as gpd
 import shapely.geometry
@@ -45,7 +50,7 @@ async def tmp_intersect_geojson():
     gdf = gdf_japan_ver821.copy()
 
     # intersections
-    area = shapely.geometry.Polygon( # TMP
+    bbox = shapely.geometry.Polygon( # TMP
         [
             (134.80, 35.2),
             (135.20, 35.2),
@@ -55,7 +60,7 @@ async def tmp_intersect_geojson():
     )
 
     # filtering
-    intersections = gdf.geometry.intersection(area)
+    intersections = gdf.geometry.intersection(bbox)
     gs_filtered = intersections[~intersections.is_empty] # geoseries
 
     # return geojson
@@ -65,5 +70,58 @@ async def tmp_intersect_geojson():
 
 
 @router.get("/japan_ver821/test0/{z}/{x}/{y}.geojson")
-async def esri_japan_ver821_test0():
-    return
+async def esri_japan_ver821_test0(
+    z: int,
+    x: int,
+    y: int,
+    KEN: Optional[str] = None,
+) -> dict:
+    """
+    ***
+    """
+    gdf = gdf_japan_ver821.copy()
+
+    # get bbox
+    nw = tile_coord(z, x, y)
+    se = tile_coord(z, x+1, y+1)
+    bbox = shapely.geometry.Polygon(
+        [
+            nw, (se[0], nw[1]),
+            se, (nw[0], se[1]), nw
+        ]
+    )
+
+    # filter by KEN
+    if KEN is not None:
+        gdf = gdf[gdf.KEN == KEN]
+
+    # filtering
+    intersections = gdf.geometry.intersection(bbox)
+    gs_filtered = intersections[~intersections.is_empty] # geoseries
+
+    # NO DATA
+    if len(gs_filtered) == 0:
+        raise HTTPException(status_code=404, detail="No Data")
+
+    # return geojson
+    return json.loads(
+        gs_filtered.to_json()
+    )
+
+
+def tile_coord(
+    zoom: int,
+    xtile: int,
+    ytile: int,
+) -> (float, float):
+    """
+    This returns the NW-corner of the square. Use the function
+    with xtile+1 and/or ytile+1 to get the other corners.
+    With xtile+0.5 & ytile+0.5 it will return the center of the tile.
+    http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#Tile_numbers_to_lon..2Flat._2
+    """
+    n = 2.0 ** zoom
+    lon_deg = xtile / n * 360.0 - 180.0
+    lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * ytile / n)))
+    lat_deg = math.degrees(lat_rad)
+    return (lon_deg, lat_deg)
